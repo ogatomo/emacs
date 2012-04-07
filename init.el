849f85d435acf4fe28e21fb13775b445cfd32ceb
t.ogawa <t.ogawa.jp@gmail.com>
2012-04-07 11:16:35 +0900

    popup,auto-complateをgitmoduleに追加

--- init.el
+++ init.el
@@ -245,6 +245,8 @@ and source-file directory for your debugger." t)
        ("\\.hpp$" (".cpp" ".c"))))
 
 ;;; auto-complete ------------------------------------------------
+(add-to-list 'load-path "~/.emacs.d/popup")
+(add-to-list 'load-path "~/.emacs.d/auto-complete")
 (require 'auto-complete-config)
 
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complate-dict")
