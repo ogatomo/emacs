2b54483e5c04070b64c20428cedf1e7fbc71047c
t.ogawa <t.ogawa.jp@gmail.com>
2012-04-07 13:39:58 +0900

    使っていないel削除

--- init.el
+++ init.el
@@ -158,19 +158,15 @@
 ))
 
 ;;; ruby     ---------------------------------------------------
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/ruby"))
+(add-to-list 'load-path "~/.emacs.d/ruby-mode")
+(require 'ruby-mode)
 
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)
-
-(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook
-          '(lambda () (inf-ruby-keys)))
-
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(autoload 'ruby-mode "ruby-mode"
+   "Mode for editing ruby source files" t)
+(setq auto-mode-alist
+   (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
+(setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
+                                interpreter-mode-alist))
 
 (autoload 'rubydb "rubydb3x"
   "run rubydb on program file in buffer *gud-file*.
