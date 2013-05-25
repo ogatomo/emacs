1cc0fd80dcfa4d49c62e4cc50213aa59e36a74a2
t.ogawa <t.ogawa.jp@gmail.com>
2013-05-25 10:37:53 +0900

    ido-mode

--- init.el
+++ init.el
@@ -299,6 +299,9 @@ and source-file directory for your debugger." t)
 (require 'rinari)
 (global-rinari-mode)
 
+(require 'ido)
+(ido-mode t)
+
 (add-to-list 'load-path "~/.emacs.d/rhtml")
 (require 'rhtml-mode)
 (add-hook 'rhtml-mode-hook
