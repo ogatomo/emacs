4bb92ecd8bb8ad5e34879b4546267a6c118e82df
t.ogawa <t.ogawa.jp@gmail.com>
2014-02-16 19:44:07 +0900

    クリップボード共有

--- init-gui.el
+++ init-gui.el
@@ -134,3 +134,17 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 (setq uniquify-ignore-buffers-re "*[^*]+}")
+
+;;; clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+  
