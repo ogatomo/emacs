ff9102103aaa8251d442df9988d54b6aea790e35
ogawa <ogawa@systemfriend.co.jp>
2014-03-04 08:17:09 +0900

    タグファイル作ってないのに聞かれるので記述を削除

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -1,30 +1,13 @@
 (require 'auto-complete)
 (require 'auto-complete-config)
-(require 'auto-complete-etags)
 
 (ac-config-default)
-
-(add-to-list 'ac-sources 'ac-source-etags)
-(setq ac-etags-use-document t)
-
-(defvar ac-source-etags
-  '((candidates . (lambda ()
-		    (all-completions ac-target (tags-completion-table))))
-    (candidate-face . ac-candidate-face)
-    (selection-face . ac-selection-face)
-    (requires . 3))
-  "etags をソースにする")
-
-(add-hook 'ruby-mode-hook
-	  (lambda ()
-	    (push 'ac-source-etags ac-sources)))
 
 (ac-set-trigger-key "TAB")
 ;;; (setq ac-auto-start nil)
 (setq ac-auto-start 1)
 (setq ac-candidate-max 40)
 
-;(setq ac-modes (append ac-modes '(objc-mode)))
 (add-to-list 'ac-modes 'objc-mode)
 (global-set-key "\M-/" 'ac-start)
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
