b4a0b628740e7c26b61d8193387e10e115b0e577
小川 智和 <t-ogawa@nijuusangou-no-iMac.local>
2014-10-27 10:04:14 +0900

    ace-searchの設定変更

--- init-search.el
+++ init-search.el
@@ -1,1 +1,8 @@
 (global-ace-isearch-mode 1)
+
+(setq ace-isearch-input-idle-delay 0.6)
+(setq ace-isearch-input-length 4)
+
+(setq ace-isearch-funtion-from-isearch 'helm-occur-from-isearch)
+
+(ace-isearch-set-ace-jump-after-isearch-exit t)
