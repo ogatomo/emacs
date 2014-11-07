e7dcd888c0f2c131d0db273883ac2ce37903d278
小川 智和 <t-ogawa@k-nakagawa.es.localnet>
2014-11-07 21:06:18 +0900

    helm-occur-from-isearchは使いづらかったのでやめる

--- init-search.el
+++ init-search.el
@@ -3,6 +3,4 @@
 (setq ace-isearch-input-idle-delay 0.6)
 (setq ace-isearch-input-length 4)
 
-(setq ace-isearch-funtion-from-isearch 'helm-occur-from-isearch)
-
 (ace-isearch-set-ace-jump-after-isearch-exit t)
